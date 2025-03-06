3b853a03dd7f2329bb3b9685394a3d2212091cf8
jayzhao65 <158057002+jayzhao65@users.noreply.github.com>
2025-03-06 17:16:21 +0800

    Initial Commit

---
+++ sourcecontrol/sourcecontrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  sourcecontrolApp.swift
+//  sourcecontrol
+//
+//  Created by Jay Zhao on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct sourcecontrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sourcecontrolTests/sourcecontrolTests.swift
@@ -0,0 +1,17 @@
+//
+//  sourcecontrolTests.swift
+//  sourcecontrolTests
+//
+//  Created by Jay Zhao on 3/6/25.
+//
+
+import Testing
+@testable import sourcecontrol
+
+struct sourcecontrolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
