1342265cbbba52bccff56c832d21197d6ff65ac8
jayzhao65 <158057002+jayzhao65@users.noreply.github.com>
2025-03-06 18:53:35 +0800

    add explore view

---
+++ sourcecontrol/featureExplore.swift
@@ -0,0 +1,18 @@
+//
+//  featureExplore.swift
+//  sourcecontrol
+//
+//  Created by Jay Zhao on 3/6/25.
+//
+
+import SwiftUI
+
+struct featureExplore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    featureExplore()
+}
