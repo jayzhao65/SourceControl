ce19c3d76d1f89fa2a2fbaf4d22349e331bd969d
jayzhao65 <158057002+jayzhao65@users.noreply.github.com>
2025-03-06 17:46:57 +0800

    add Homeview

---
+++ sourcecontrol/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  sourcecontrol
+//
+//  Created by Jay Zhao on 3/6/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
